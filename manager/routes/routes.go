a725cf01ce0fd0bbff7afe41602ad76d2f3983ee
sentiers <sentiersg@gmail.com>
2022-01-05 15:27:21 +0900

    Set relationship

--- manager/routes/routes.go
+++ manager/routes/routes.go
@@ -9,24 +9,28 @@ import (
 func Routers() *gin.Engine {
 	r := gin.Default()
 
+	// company
 	r.GET("company", controller.GetAllCompanies)
 	r.POST("company", controller.CreateCompany)
 	r.GET("company/:id", controller.GetCompanyById)
 	r.PUT("company/:id", controller.UpdateCompany)
 	r.DELETE("company/:id", controller.DeleteCompany)
 
+	// department
 	r.GET("department", controller.GetAllDepartments)
 	r.POST("department", controller.CreateDepartment)
 	r.GET("department/:id", controller.GetDepartmentById)
 	r.PUT("department/:id", controller.UpdateDepartment)
 	r.DELETE("department/:id", controller.DeleteDepartment)
 
+	// team
 	r.GET("team", controller.GetAllTeams)
 	r.POST("team", controller.CreateTeam)
 	r.GET("team/:id", controller.GetTeamById)
 	r.PUT("team/:id", controller.UpdateTeam)
 	r.DELETE("team/:id", controller.DeleteTeam)
 
+	// employee
 	r.GET("employee", controller.GetAllEmployees)
 	r.POST("employee", controller.CreateEmployee)
 	r.GET("employee/:id", controller.GetEmployeeById)
