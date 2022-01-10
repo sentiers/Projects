43ff81048b6149526fca632d4d2fcd5a701bd81d
sentiers <sentiersg@gmail.com>
2022-01-10 15:57:22 +0900

    Reach 100% of test coverage for middleware package

--- API_project/middleware/middleware_test.go
+++ API_project/middleware/middleware_test.go
@@ -1,108 +1,102 @@
 package middleware
 
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"manager/admin"
-// 	"manager/config"
-// 	"manager/controllers"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
+import (
+	"fmt"
+	"manager/admin"
+	"manager/config"
+	"manager/controllers"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestAuthzNoHeader(t *testing.T) {
-// 	router := gin.Default()
-// 	router.Use(Authz())
+func TestAuthzNoHeader(t *testing.T) {
+	router := gin.Default()
+	router.Use(Authz())
 
-// 	router.GET("/api/protected/profile", controllers.Profile)
+	router.GET("/company", controllers.GetAllCompanies)
 
-// 	w := httptest.NewRecorder()
-// 	req, _ := http.NewRequest("GET", "/api/protected/profile", nil)
-// 	router.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/company", nil)
+	router.ServeHTTP(w, req)
 
-// 	assert.Equal(t, 403, w.Code)
-// }
+	assert.Equal(t, 403, w.Code)
+}
 
-// func TestAuthzInvalidTokenFormat(t *testing.T) {
-// 	router := gin.Default()
-// 	router.Use(Authz())
+func TestAuthzInvalidTokenFormat(t *testing.T) {
+	router := gin.Default()
+	router.Use(Authz())
 
-// 	router.GET("/api/protected/profile", controllers.Profile)
+	router.GET("/company", controllers.GetAllCompanies)
 
-// 	w := httptest.NewRecorder()
-// 	req, _ := http.NewRequest("GET", "/api/protected/profile", nil)
-// 	req.Header.Add("Authorization", "test")
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/company", nil)
+	req.Header.Add("Authorization", "test")
 
-// 	router.ServeHTTP(w, req)
+	router.ServeHTTP(w, req)
 
-// 	assert.Equal(t, 400, w.Code)
-// }
+	assert.Equal(t, 400, w.Code)
+}
 
-// func TestAuthzInvalidToken(t *testing.T) {
-// 	invalidToken := "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-// 	router := gin.Default()
-// 	router.Use(Authz())
+func TestAuthzInvalidToken(t *testing.T) {
+	invalidToken := "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+	router := gin.Default()
+	router.Use(Authz())
 
-// 	router.GET("/api/protected/profile", controllers.Profile)
+	router.GET("/company", controllers.GetAllCompanies)
 
-// 	w := httptest.NewRecorder()
-// 	req, _ := http.NewRequest("GET", "/api/protected/profile", nil)
-// 	req.Header.Add("Authorization", invalidToken)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/company", nil)
+	req.Header.Add("Authorization", invalidToken)
 
-// 	router.ServeHTTP(w, req)
+	router.ServeHTTP(w, req)
 
-// 	assert.Equal(t, 401, w.Code)
-// }
+	assert.Equal(t, 401, w.Code)
+}
 
-// func TestValidToken(t *testing.T) {
-// 	var response admin.User
+func TestValidToken(t *testing.T) {
 
-// 	err := config.InitDatabase()
-// 	assert.NoError(t, err)
+	err := config.InitDatabase()
+	assert.NoError(t, err)
 
-// 	err = config.DB.AutoMigrate(&admin.User{})
-// 	assert.NoError(t, err)
+	err = config.DB.AutoMigrate(&admin.User{})
+	assert.NoError(t, err)
 
-// 	user := admin.User{
-// 		Email:    "test@email.com",
-// 		Password: "secret",
-// 	}
+	user := admin.User{
+		Email:    "test@email.com",
+		Password: "secret",
+	}
 
-// 	jwtWrapper := admin.JwtWrapper{
-// 		SecretKey:       "verysecretkey",
-// 		Issuer:          "AuthService",
-// 		ExpirationHours: 24,
-// 	}
+	jwtWrapper := admin.JwtWrapper{
+		SecretKey:       "verysecretkey",
+		Issuer:          "Alchera",
+		ExpirationHours: 24,
+	}
 
-// 	token, err := jwtWrapper.GenerateToken(user.Email)
-// 	assert.NoError(t, err)
+	token, err := jwtWrapper.GenerateToken(user.Email)
+	assert.NoError(t, err)
 
-// 	err = user.HashPassword(user.Password)
-// 	assert.NoError(t, err)
+	err = user.HashPassword(user.Password)
+	assert.NoError(t, err)
 
-// 	result := config.DB.Create(&user)
-// 	assert.NoError(t, result.Error)
+	result := config.DB.Create(&user)
+	assert.NoError(t, result.Error)
 
-// 	router := gin.Default()
-// 	router.Use(Authz())
+	router := gin.Default()
+	router.Use(Authz())
 
-// 	router.GET("/api/protected/profile", controllers.Profile)
+	router.GET("/company", controllers.GetAllCompanies)
 
-// 	w := httptest.NewRecorder()
-// 	req, _ := http.NewRequest("GET", "/api/protected/profile", nil)
-// 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/company", nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 
-// 	router.ServeHTTP(w, req)
+	router.ServeHTTP(w, req)
 
-// 	err = json.Unmarshal(w.Body.Bytes(), &response)
-// 	assert.NoError(t, err)
+	assert.Equal(t, 200, w.Code)
 
-// 	assert.Equal(t, 200, w.Code)
-// 	assert.Equal(t, "test@email.com", response.Email)
-
-// 	config.DB.Unscoped().Where("email = ?", user.Email).Delete(&admin.User{})
-// }
+	config.DB.Unscoped().Where("email = ?", user.Email).Delete(&admin.User{})
+}
